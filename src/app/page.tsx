183ca89c47a07cd9ee1b940c2e972ac9676dead3
Aditya Arora <aroraaditya411@gmail.com>
2025-10-01 14:50:40 +0000

    decrease the top padding sliglty so that all the elemnst are centreredin

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -221,21 +221,21 @@ export default function Home() {
   const currentImageUrl = generatedImageUrls.length > 0 ? generatedImageUrls[currentImageIndex] : null;
 
   return (
-    <div className="relative h-screen flex flex-col p-4 sm:p-6 md:p-8 font-body text-foreground">
+    <div className="relative h-screen flex flex-col pt-2 px-4 pb-4 sm:px-6 sm:pb-6 md:px-8 md:pb-8 font-body text-foreground">
 
       <header className="text-center py-2">
         <div className="flex items-center justify-center space-x-2">
            <svg
               xmlns="http://www.w3.org/2000/svg"
               viewBox="0 0 100 100"
-              className="h-12 w-12 text-primary"
+              className="h-14 w-14 text-primary"
               fill="currentColor"
             >
               <path d="M50 0L50 25L12.5 25L12.5 37.5L50 37.5L50 62.5L12.5 62.5L12.5 75L50 75L50 100L87.5 100L87.5 0L50 0Z" />
             </svg>
-            <span className="text-4xl font-semibold"> Mocksy</span>
+            <span className="text-5xl font-semibold"> Mocksy</span>
         </div>
-        <p className="mt-1 text-muted-foreground text-base font-regular">
+        <p className="mt-1 text-muted-foreground text-lg font-regular">
           AI-Powered Brand Identity Mockups Generator
         </p>
       </header>
